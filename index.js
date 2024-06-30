5b7eb77b9a7df61db59e13cda13ac7818b6f33bc
Rabbi hossain <rh7391679@gmail.com>
2024-06-30 12:55:34 -0700

    add a

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+var cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
-
 const port = process.env.PORT || 5000
 
 // middleware
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173'],
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 
 
@@ -23,6 +27,10 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -32,7 +40,19 @@ async function run() {
         const bookingsCollection = client.db('carDoctor').collection('bookings')
 
 
-        app.get('/services', async (req, res) => {
+        // auth related api
+        app.post('/jwt', async (req, res)=>{
+            const user = req.body
+            console.log(user)
+            const token = jwt.sign(user, 'secret', {expiresIn: '1h'})
+            res.send(token)
+        })
+
+
+
+
+        // Service related api
+        app.get('/services',  async (req, res) => {
             const cursor = serviceCollection.find()
             const result = await cursor.toArray()
             res.send(result)
@@ -53,12 +73,12 @@ async function run() {
 
         // bookings
 
-        app.get('/bookings', async(req, res)=>{
-            console.log(req.query.email);
-            let query = {};
-            if(req.query?.email) {
-                query = {email: req.query.email}
-            }
+        app.get('/bookings',  async(req, res)=>{
+          
+            // let query = {};
+            // if(req.query?.email) {
+            //     query = {email: req.query.email}
+            // }
             const result = await bookingsCollection.find(query).toArray()
             res.send(result)
         })
